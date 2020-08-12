6a79b345cc19e4a342564546111ae346016379bd
Vic <yangyuze1018@hotmail.com>
2020-08-12 22:46:50 +1000

    add basename.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Router>
+		<Router basename={window.location.pathname || ''}>
 			<App />
 		</Router>
 	</React.StrictMode>,
