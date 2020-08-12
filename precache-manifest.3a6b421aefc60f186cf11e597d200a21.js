f2c05c9c5affda0392d488bd28c421e847a11c3b
Vic <yangyuze1018@hotmail.com>
2020-08-12 22:23:06 +1000

    Updates

---
+++ precache-manifest.3a6b421aefc60f186cf11e597d200a21.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4893725104a58502c6f395d99a48c5d",
+    "url": "/portfolio-create-react-app/index.html"
+  },
+  {
+    "revision": "59ef6e7a895a5ce188da",
+    "url": "/portfolio-create-react-app/static/css/main.6f103955.chunk.css"
+  },
+  {
+    "revision": "51137ca48df826440ba9",
+    "url": "/portfolio-create-react-app/static/js/2.3bc057b0.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/portfolio-create-react-app/static/js/2.3bc057b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ef6e7a895a5ce188da",
+    "url": "/portfolio-create-react-app/static/js/main.b0b6d010.chunk.js"
+  },
+  {
+    "revision": "81358f478c2b76193e1c",
+    "url": "/portfolio-create-react-app/static/js/runtime-main.0207ea86.js"
+  },
+  {
+    "revision": "49b81deba42911926c4d7c86482cd76d",
+    "url": "/portfolio-create-react-app/static/media/Avatar-Maker.49b81deb.png"
+  },
+  {
+    "revision": "7d2385f38bafbb2864a3324685942876",
+    "url": "/portfolio-create-react-app/static/media/project1.7d2385f3.png"
+  },
+  {
+    "revision": "edfcbdd86ada1b24b86741fdfe598a1d",
+    "url": "/portfolio-create-react-app/static/media/project2.edfcbdd8.jpg"
+  },
+  {
+    "revision": "738e52599c74e8c76d5ec64f98226bb0",
+    "url": "/portfolio-create-react-app/static/media/project3.738e5259.jpg"
+  },
+  {
+    "revision": "547a87b6d08a9062f533d290ea754291",
+    "url": "/portfolio-create-react-app/static/media/project4.547a87b6.jpg"
+  },
+  {
+    "revision": "9c78c2cb3c409b107816a67aa10d8e9e",
+    "url": "/portfolio-create-react-app/static/media/tech.9c78c2cb.jpg"
+  }
+]);
